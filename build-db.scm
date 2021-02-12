8618b9b47a675e1ece49c015f9033b2d1a0ddf3d
George Chan <okcy1016@gmail.com>
2021-02-12 15:52:21 +0800

    finished db builder

---
+++ build-db.scm
@@ -0,0 +1,68 @@
+(define poem-archive-uri "https://github.com/chinese-poetry/chinese-poetry/archive/master.zip")
+(define tmp-dir "./tmp")
+(define poem-archive-filepath "./tmp/chinese-poetry-master.zip")
+(define poem-db-path "./poems.db")
+
+(define get-poet-tang-filename-list
+  (lambda (dir)
+    (use-modules (ice-9 ftw))
+    (let ((filenames-in-dir (scandir dir)))
+      (filter (lambda (item)
+                (string-prefix? "poet.tang" item))
+              filenames-in-dir))))
+
+(define get-poet-tang-filepath-list
+  (lambda (dir)
+    (let ((poet-tang-filename-list (get-poet-tang-filename-list dir)))
+      (map
+       (lambda (dir filename) (string-append dir filename))
+       (make-list (length poet-tang-filename-list) dir)
+       poet-tang-filename-list))))
+
+;; create tmp dir if not exists
+(if (not (access? tmp-dir F_OK))
+    (mkdir tmp-dir))
+
+;; download poem if not exists
+(if (not (access? poem-archive-filepath F_OK))
+    (begin
+      (display "first time use, downloading poems from GitHub ...")
+      (newline)
+      (load-extension "./net-helper" "init_poem_net_helper")
+      (poem-download-archive poem-archive-uri poem-archive-filepath)
+      ;; extract archive
+      (display "extracting archive ...")
+      (newline)
+      ;; which os?
+      (if (string=? (vector-ref (uname) 0) "Linux")
+          (system (string-append
+                   "unzip"
+                   "-q"
+                   " "
+                   poem-archive-filepath
+                   " "
+                   "-d"
+                   " "
+                   tmp-dir))
+          (begin
+            (display "win32 or macos are not supported currently.\n")
+            (quit)))))
+
+;;;; insert poems to db
+;; delete db first
+(if (access? poem-db-path F_OK)
+    (delete-file poem-db-path))
+
+(load-extension "./db-helper" "init_poem_db_helper")
+(use-modules (ice-9 format))
+(for-each
+ (lambda (elem)
+   (display
+    (format
+     #f
+     "~d poems inserted from ~a\n"
+     (poem-json2db elem poem-db-path)
+     elem)))
+ (get-poet-tang-filepath-list "./tmp/chinese-poetry-master/json/"))
+
+(display (format #f "generated ~a\n" poem-db-path))
---
+++ db-helper.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <sqlite3.h>
+#include <libguile.h>
+#include <json-c/json.h>
+
+struct poem {
+  const char* title;
+  const char* author;
+  const char* paragraphs;
+};
+
+int loop_poem_and_insert(json_object*, const char*);
+SCM read_poem_2_db(SCM, SCM);
+void init_poem_db_helper();
+int insert_single_poem(sqlite3*, struct poem*);
+static int callback(void*, int, char**, char**);
+char* build_insert_sql(struct poem*);
+
+static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+  int i;
+  for(i=0; i<argc; i++){
+    printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+  }
+  printf("\n");
+  return 0;
+}
+
+SCM read_poem_2_db(SCM scm_jsonpath, SCM scm_dbpath) {
+  /* char* is a mutable pointer to a mutable character/string. */
+
+  /* const char* is a mutable pointer to an immutable character/string. */
+  /* You cannot change the contents of the location(s) this pointer points to. */
+  /* Also, compilers are required to give error messages when you try to do so. */
+  /* For the same reason, conversion from const char * to char* is deprecated. */
+  
+  /* char* const is an immutable pointer (it cannot point to any other location) */
+  /* but the contents of location at which it points are mutable. */
+
+  /* const char* const is an immutable pointer to an immutable character/string. */    
+  const char *json_path = scm_to_locale_string(scm_jsonpath);
+  const char *db_path = scm_to_locale_string(scm_dbpath);
+  json_object* root = json_object_from_file(json_path);
+
+  int poem_count = loop_poem_and_insert(root, db_path);
+  
+  json_object_put(root);
+  return scm_from_int(poem_count);
+}
+
+void init_poem_db_helper() {
+  scm_c_define_gsubr("poem-json2db", 2, 0, 0, read_poem_2_db);
+}
+
+int loop_poem_and_insert(json_object* root, const char* db_path) {
+  sqlite3* db;
+  int poem_count;
+  int para_count;
+  json_object* poem_elem;
+  json_object* tmp_elem;
+  json_object* para_elem;
+  struct poem* poem = malloc(sizeof(struct poem));
+  // must be initialized, or `\377\377\377` may appear
+  // an uninitialized character array may preforms an out of bounds access
+  // see https://stackoverflow.com/questions/43384991/different-ways-to-initialize-char-array-are-they-correct
+  // fixme: may encounter segment fault if the buffer size is too small
+  char buf[1024*32] = {0};
+
+  poem_count = json_object_array_length(root);
+
+  // open db
+  int rc = sqlite3_open(db_path, &db);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "can't open database: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return -1;
+  }
+
+  // create `poem` table
+  const char sql[256] = "CREATE TABLE poem (title string, author string, paragraphs string);";
+  sqlite3_exec(db, sql, callback, 0, NULL);
+
+  // wrap in one transaction, speed up bulk insert!
+  // see https://stackoverflow.com/questions/1711631/improve-insert-per-second-performance-of-sqlite
+  sqlite3_exec(db, "BEGIN TRANSACTION", NULL, NULL, NULL);
+  
+  for(int i=0; i<poem_count; i++) {
+    poem_elem = json_object_array_get_idx(root, i);
+    
+    json_object_object_get_ex(poem_elem, "title", &tmp_elem);
+    poem->title = json_object_get_string(tmp_elem);
+    
+    json_object_object_get_ex(poem_elem, "author", &tmp_elem);
+    poem->author = json_object_get_string(tmp_elem);
+    
+    json_object_object_get_ex(poem_elem, "paragraphs", &tmp_elem);
+    para_count = json_object_array_length(tmp_elem);
+    
+    for(int j=0; j<para_count; j++) {
+      para_elem = json_object_array_get_idx(tmp_elem, j);
+      strcat(buf, json_object_get_string(para_elem));
+      if (j != (para_count - 1))
+        strcat(buf, "\n");
+    }
+    poem->paragraphs = buf;
+    insert_single_poem(db, poem);
+    memset(buf, 0, sizeof(buf));
+  }
+
+  // end sqlite transaction
+  sqlite3_exec(db, "END TRANSACTION", NULL, NULL, NULL);
+  
+  json_object_put(tmp_elem);
+  json_object_put(poem_elem);
+  json_object_put(para_elem);
+  free(poem);
+  sqlite3_close(db);
+  return poem_count;
+}
+
+int insert_single_poem(sqlite3* db, struct poem* poem) {  
+  char* zErrMsg = 0;
+
+  char* sql = build_insert_sql(poem);
+  /* printf("sql: %s\n", sql); */
+  int rc = sqlite3_exec(db, sql, callback, 0, &zErrMsg);
+  if(rc != SQLITE_OK){
+    fprintf(stderr, "sql error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  free(sql);
+  
+  return 0;
+}
+
+char* build_insert_sql(struct poem* poem) {
+  // fixme: may encounter segment fault if the memory size is too small
+  char* sql = malloc(1024*32);
+  sprintf(sql, "INSERT INTO poem VALUES (\"%s\", \"%s\", \"%s\");", poem->title, poem->author, poem->paragraphs);
+  return sql;
+}
